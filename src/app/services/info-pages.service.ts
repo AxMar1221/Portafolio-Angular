67992c95e526492569c6f6006634b69cd8e13204
Tachi Dito <tachidito_7inc@hotmail.com>
2022-07-05 23:57:05 -0500

    fire base

--- src/app/services/info-pages.service.ts
+++ src/app/services/info-pages.service.ts
@@ -10,16 +10,34 @@ export class InfoPagesService {
   info: InfoPage = {};
   loade: boolean = false;
 
+  team: any[] = []
+
   constructor( private http: HttpClient ) { 
     // console.log('Info page loade')
-
+    this.loadeInfo();
+    this.loadeTeam();
+  }
+  
+  private loadeInfo() {
     //read Json
     this.http.get('assets/data/data-page.json')
-      .subscribe( (resp: InfoPage) => {
-
-        this.loade = true,
-        this.info = resp;
-        console.log( resp );
-      });
+    .subscribe( (resp: InfoPage) => {
+      
+      this.loade = true,
+      this.info = resp;
+      // console.log( resp ); 
+    });
+  }
+  
+  private loadeTeam() {
+    
+    this.http.get('https://angular-html-2482a-default-rtdb.firebaseio.com/Equipo.json')
+    .subscribe( ( resp: any) => {
+      
+      this.team = resp;
+      // this.loade = true,
+      // this.info = resp,
+      console.log( resp );
+    })
   }
 }
